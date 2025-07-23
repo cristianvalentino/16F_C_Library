ff2447640844f66f2027438af331b5a85a382e2f
Cristian D. Valentino <36926239+cristianvalentino@users.noreply.github.com>
2025-07-23 19:10:11 -0300

    actualizacion

--- PIC16F18877_timer2.h
+++
@@ -1,8 +0,0 @@
-#ifndef PIC16F8877_timer2_H
-#define PIC16F8877_timer2_H
-
-	void TMR2_Initialize(void);
-	uint32_t Ticks(void);
-	void TMR2_ISR(void)
-
-#endif
--- PIC16F8877_interrupt.h
+++
@@ -1,4 +0,0 @@
-#ifndef PIC16F8877_interrupt_H
-#define PIC16F8877_interrupt_H
-	
-#endif
